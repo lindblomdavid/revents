7a77c541dd96d411d03636f87165b45f0668c117
David Lindblom <daavidlindblom@gmail.com>
2021-12-15 16:19:32 +0100

    second commit

---
+++ src/features/nav/NavBar.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Button, Container, Menu } from "semantic-ui-react";
+
+
+export default function NavBar() {
+  return (
+    <Menu inverted fixed='top'>
+      <Container>
+<Menu.Item header>
+  <img src="/assets/logo.png" alt="logo"style={{marginRight: 15}}/>
+  Re-vents
+</Menu.Item>
+    <Menu.Item name='Events'/>
+    <Menu.Item>
+    <Button postivie inverted content = 'Skapa träningstillfälle'/>
+    </Menu.Item>
+
+    <Menu.Item position='right'>
+
+    <Button basic inverted content ='Logga in'/>
+    <Button basic inverted content ='Registrera dig' style ={{marginLef:'0.5em'}}/>
+
+    </Menu.Item>
+      </Container>
+    </Menu>
+  )
+}
