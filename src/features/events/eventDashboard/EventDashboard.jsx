3b74177bdf010e999718577294b3ead74c2c498e
David Lindblom <daavidlindblom@gmail.com>
2021-12-16 08:23:36 +0100

    Static page

--- src/features/events/eventDashboard/EventDashboard.jsx
+++ src/features/events/eventDashboard/EventDashboard.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { Grid } from 'semantic-ui-react';
 import EventForm from '../eventForm/eventForm';
 import EventList from './EventList';
+import  {sampleData} from '../../../app/api/sampleData.js';
 
 export default function EventDashboard() {
 
   return (
     <Grid>
 <Grid.Column width={10}>
-<EventList />
+<EventList events={sampleData} />
 </Grid.Column>
 <Grid.Column width={6}>
 <EventForm/>
