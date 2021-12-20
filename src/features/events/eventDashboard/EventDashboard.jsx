feff2554a76ac94b197f07b824e520670befd24a
David Lindblom <daavidlindblom@gmail.com>
2021-12-20 09:06:04 +0100

    added delete and update
    added delete and update

--- src/features/events/eventDashboard/EventDashboard.jsx
+++ src/features/events/eventDashboard/EventDashboard.jsx
@@ -1,27 +1,53 @@
-import React, {useState} from 'react';
-import { Grid } from 'semantic-ui-react';
-import EventForm from '../eventForm/eventForm';
-import EventList from './EventList';
-import  {sampleData} from '../../../app/api/sampleData.js';
+import React, { useState } from "react";
+import { Grid } from "semantic-ui-react";
+import EventForm from "../eventForm/eventForm";
+import EventList from "./EventList";
+import { sampleData } from "../../../app/api/sampleData.js";
 
-export default function EventDashboard({formOpen, setFormOpen}) {
-const [events, setEvents] = useState(sampleData);
+export default function EventDashboard({
+  formOpen,
+  setFormOpen,
+  selectEvent,
+  selectedEvent,
+}) {
+  const [events, setEvents] = useState(sampleData);
 
+  function handleCreateEvent(event) {
+    setEvents([...events, event]);
+  }
 
+  function handleUpdateEvent(updatedEvent) {
+    setEvents(
+      events.map((evt) => (evt.id === updatedEvent.id ? updatedEvent : evt))
+    );
+    selectEvent(null);
+  }
 
+  function handleDeleteEvent(eventId) {
+    setEvents(events.filter((evt) => evt.id !== eventId));
+  }
 
   return (
     <Grid>
-<Grid.Column width={10}>
-<EventList events={events} />
-</Grid.Column>
-<Grid.Column width={6}>
-  {formOpen &&
-<EventForm setFormOpen={setFormOpen}/>}
-</Grid.Column>
-
+      <Grid.Column width={10}>
+        <EventList
+          events={events}
+          selectEvent={selectEvent}
+          deleteEvent={handleDeleteEvent}
+        />
+      </Grid.Column>
+      <Grid.Column width={6}>
+        {formOpen && (
+          <EventForm
+            setFormOpen={setFormOpen}
+            setEvents={setEvents}
+            createEvent={handleCreateEvent}
+            selectedEvent={selectedEvent}
+            updateEvent={handleUpdateEvent}
+            key={selectedEvent ? selectedEvent.id : null}
+          />
+        )}
+      </Grid.Column>
     </Grid>
-  )
-
+  );
 }
-
--- src/features/events/eventDashboard/EventListItem.jsx
+++ src/features/events/eventDashboard/EventListItem.jsx
@@ -1,48 +1,49 @@
-import React from 'react';
-import { Button, Icon, Item, List, Segment } from 'semantic-ui-react';
-import EventListAttendee from './EventListAttendee';
+import React from "react";
+import { Button, Icon, Item, List, Segment } from "semantic-ui-react";
+import EventListAttendee from "./EventListAttendee";
 
-export default function EventListItem({event}){
-
+export default function EventListItem({ event, selectEvent, deleteEvent }) {
   return (
     <Segment.Group>
       <Segment>
         <Item.Group>
           <Item>
-            <Item.Image size='tiny' circular src={event.hostPhotoURL}/>
-              <Item.Content>
-                <Item.Header content ={event.title}/>
-                <Item.Description>Hosted By {event.hostedBy}</Item.Description>
-              </Item.Content>
+            <Item.Image size='tiny' circular src={event.hostPhotoURL} />
+            <Item.Content>
+              <Item.Header content={event.title} />
+              <Item.Description>Hosted By {event.hostedBy}</Item.Description>
+            </Item.Content>
           </Item>
-
         </Item.Group>
-
       </Segment>
       <Segment>
-      <span>
-        <Icon name='clock'/> {event.date}
-        <Icon name='clock'/> {event.venue}
-      </span>
+        <span>
+          <Icon name='clock' /> {event.date}
+          <Icon name='clock' /> {event.venue}
+        </span>
       </Segment>
-    <Segment secondary>
-    <List horizontal>
-      {event.attendees.map(attendee => (
-        <EventListAttendee key={attendee.id} attendee={attendee} />
-
-      ) )}
-      
-      
-    </List>
-    <Segment clearing>
-      <div>{event.description}</div>
-      <Button color='teal' floated='right' content ='View'/>
-
-    </Segment>
-
-
-    </Segment>
+      <Segment secondary>
+        <List horizontal>
+          {event.attendees.map((attendee) => (
+            <EventListAttendee key={attendee.id} attendee={attendee} />
+          ))}
+        </List>
+        <Segment clearing>
+          <div>{event.description}</div>
+          <Button
+            onClick={() => deleteEvent(event.id)}
+            color='red'
+            floated='right'
+            content='Delete'
+          />
+          <Button
+            onClick={() => selectEvent(event)}
+            color='teal'
+            floated='right'
+            content='View'
+          />
+        </Segment>
+      </Segment>
     </Segment.Group>
-  )
-
+  );
 }
